88c262f2d208dffadb1e57c1f4e52db149c514ce
Mateus Pereira <mbpereira98@gmail.com>
2023-02-18 18:48:58 -0300

    adicionando npgsql provider para ef core

---
+++ src/Pan.Affiliation.Infrastructure/Data/PanAffiliationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Pan.Affiliation.Infrastructure.Data
+{
+    public class PanAffiliationDbContext : DbContext
+    {
+        public PanAffiliationDbContext()
+        {
+        }
+
+        public PanAffiliationDbContext(DbContextOptions<DbContext> opt)
+            : base(opt) { }
+    }
+}
